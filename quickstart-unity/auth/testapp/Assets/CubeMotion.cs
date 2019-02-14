293db29b24a01975dba86f5ddc5ef249770693d1
tua58997 <tua58997@temple.edu>
2019-02-14 10:25:11 -0500

    added quickstart-unity dir

---
+++ quickstart-unity/auth/testapp/Assets/CubeMotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMotion : MonoBehaviour
+{
+    public Rigidbody rb; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddTorque(0, 1 * Time.deltaTime, 1 * Time.deltaTime);
+    }
+
+}
