81c8d02eeaa164c5967b41c487a6be6841acb83f
tua58997 <tua58997@temple.edu>
2019-02-18 19:50:23 -0500

    adding photon tutorial project. still has some bugs

---
+++ Photon Tutorial Project/Assets/PlayerManager.cs
@@ -0,0 +1,299 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+using System.Collections;
+using Photon.Pun;
+
+namespace Com.Kabaj.PhotonTutorialProject
+{
+    /// <summary>
+    /// Player manager.
+    /// Handles fire Input and Beams.
+    /// </summary>
+    public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
+    {
+
+        // Note: I changed this from "Private Fields" to "Public Fields" because it didn't make any sense to me
+        #region Public Fields
+
+
+        [Tooltip("The current Health of our player")]
+        public float Health = 1f;
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+        [Tooltip("The Player's UI GameObject Prefab")]
+        [SerializeField]
+        public GameObject PlayerUiPrefab;
+
+
+        #endregion
+
+
+        #region Private Fields
+
+
+        [Tooltip("The Beams GameObject to control")]
+        [SerializeField]
+        private GameObject beams;
+        //True, when the user is firing
+        bool IsFiring;
+
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        /// </summary>
+        void Awake()
+        {
+            if (beams == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> Beams Reference.", this);
+            }
+            else
+            {
+                beams.SetActive(false);
+            }
+
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        void Start()
+        {
+            // Note from tutorial:
+            // First, it gets the CameraWork component, we expect this, so if we don't find it, we log an error.
+            CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork>();
+
+
+            if (_cameraWork != null)
+            {
+                /** Note from tutorial: 
+                 * if photonView.IsMine is true, it means we need to follow this instance, and so we call _cameraWork.OnStartFollowing() 
+                 * which effectivly makes the camera follow that very instance in the scene. All other player instances will have their 
+                 * photonView.IsMine set as false, and so their respective _cameraWork will do nothing.
+                 */
+                if (photonView.IsMine)
+                {
+                    // Note from tutorial:
+                    // makes the camera follow that very instance in the scene.
+                    _cameraWork.OnStartFollowing();
+                }
+            }
+            else
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+            }
+
+            /** Notes from tutorial:
+             * There is currently an added complexity because Unity has revamped "Scene Management" and Unity 5.4 has deprecated 
+             * some callbacks, which makes it slightly more complex to create a code that works across all Unity versions (from Unity 
+             * 5.3.7 to the latest). So we'll need different code based on the Unity version. It's unrelated to Photon Unity Networking, but 
+             * important to master for your projects to survive updates.
+             */
+#if UNITY_5_4_OR_NEWER
+            // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += (scene, loadingMode) => // I'm guessing this creates an anonymous function with parameters scene and loadingMode
+            {
+                this.CalledOnLevelWasLoaded(scene.buildIndex);
+            };
+#endif
+
+            /** Notes from tutorial:
+             * All of this is standard Unity coding. However notice that we are sending a message to the instance we've just created. We 
+             * require a receiver, which means we will be alerted if the SetTarget did not find a component to respond to it. Another 
+             * way would have been to get the PlayerUI component from the instance, and then call SetTarget directly. It's generally 
+             * recommended to use components directly, but it's also good to know you can achieve the same thing in various ways.
+             */
+            if (PlayerUiPrefab != null)
+            {
+                // I added  if (photonView.IsMine) to fix a bug where the local player would see both players' UIs
+                // This code is also CalledOnLevelWasLoaded()
+                //if (photonView.IsMine)
+                //{
+                    GameObject _uiGo = Instantiate(PlayerUiPrefab);
+                    _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+                //}
+            }
+            else
+                {
+                    Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
+                }
+
+        }
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity on every frame.
+        /// </summary>
+        void Update()
+        {
+            if (photonView.IsMine)
+            {
+                ProcessInputs();
+            }
+
+            if (Health <= 0f)
+            {
+                GameManager.Instance.LeaveRoom();
+            }
+
+            // trigger Beams active state
+            if (beams != null && IsFiring != beams.activeSelf)
+            {
+                beams.SetActive(IsFiring);
+            }
+        }
+
+        /// <summary>
+        /// MonoBehaviour method called when the Collider 'other' enters the trigger.
+        /// Affect Health of the Player if the collider is a beam
+        /// Note: when jumping and firing at the same, you'll find that the player's own beam intersects with itself
+        /// One could move the collider further away to prevent this or check if the beam belongs to the player.
+        /// </summary>
+        void OnTriggerEnter(Collider other)
+        {
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+            // We are only interested in Beamers
+            // we should be using tags but for the sake of distribution, let's simply check by name.
+            if (!other.name.Contains("Beam"))
+            {
+                return;
+            }
+            Health -= 0.1f;
+        }
+       
+        /// <summary>
+        /// MonoBehaviour method called once per frame for every Collider 'other' that is touching the trigger.
+        /// We're going to affect health while the beams are touching the player
+        /// </summary>
+        /// <param name="other">Other.</param>
+        void OnTriggerStay(Collider other)
+        {
+            // we dont' do anything if we are not the local player.
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+            // We are only interested in Beamers
+            // we should be using tags but for the sake of distribution, let's simply check by name.
+            if (!other.name.Contains("Beam"))
+            {
+                return;
+            }
+            // we slowly affect health when beam is constantly hitting us, so player has to move to prevent death.
+            Health -= 0.1f * Time.deltaTime;
+        }
+
+        // This won't be called on my system because it's newer than 5.4
+ #if !UNITY_5_4_OR_NEWER
+        /// <summary>See CalledOnLevelWasLoaded. Outdated in Unity 5.4.</summary>
+        void OnLevelWasLoaded(int level)
+        {
+            this.CalledOnLevelWasLoaded(level);
+        }
+#endif
+        
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            /** Note from tutorial:
+             *   raycast downwards the current player's position to see 
+             *   if we hit anything. If we don't, this is means we are not above the arena's ground and we need to be repositioned back to 
+             *   the center, exactly like when we are entering the room for the first time.
+             */
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+
+            /** Note from tutorial:
+             *   when a new level is loaded, the UI is being 
+             *   destroyed yet our player remains... so we need to instantiate it as well when we know a level was loaded
+             *   ...
+             *   Note that there are more complex/powerful ways to deal with this and the UI could be made out with a singleton, but it 
+             *   would quickly become complex, because other players joining and leaving the room would need to deal with their UI as 
+             *   well. In our implementation, this is straight forward, at the cost of a duplication of where we instantiate our UI prefab. As a 
+             *   simple exercise, you can create a private method that would instantiate and send the "SetTarget" message, and from the 
+             *   various places, call that method instead of duplicating the code.
+             */
+            // I added  if (photonView.IsMine) to fix a bug where the local player would see both players' UIs
+            // This code is also Start()
+            //if (photonView.IsMine) 
+            //{
+                GameObject _uiGo = Instantiate(this.PlayerUiPrefab);
+                _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+            //}
+        }
+
+
+        #endregion
+
+
+        #region IPunObservable implementation
+
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                // We own this player: send the others our data
+                stream.SendNext(IsFiring);
+                stream.SendNext(Health);
+            }
+            else
+            {
+                // Network player, receive data
+                this.IsFiring = (bool)stream.ReceiveNext();
+                this.Health = (float)stream.ReceiveNext();
+            }
+        }
+
+
+        #endregion
+
+
+        #region Custom
+
+
+        /// <summary>
+        /// Processes the inputs. Maintain a flag representing when the user is pressing Fire.
+        /// </summary>
+        void ProcessInputs()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (!IsFiring)
+                {
+                    IsFiring = true;
+                }
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                if (IsFiring)
+                {
+                    IsFiring = false;
+                }
+            }
+        }
+
+
+        #endregion
+    }
+}
---
+++ Photon Tutorial Project/Assets/PlayerUI.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+using System.Collections;
+
+
+namespace Com.Kabaj.PhotonTutorialProject
+{
+    public class PlayerUI : MonoBehaviour
+    {
+        #region Private Fields
+
+
+        [Tooltip("UI Text to display Player's Name")]
+        [SerializeField]
+        private Text playerNameText;
+
+
+        [Tooltip("UI Slider to display Player's Health")]
+        [SerializeField]
+        private Slider playerHealthSlider;
+
+        [Tooltip("Pixel offset from the player target")]
+        [SerializeField]
+        private Vector3 screenOffset = new Vector3(0f, 30f, 0f);
+
+        /** Note from tutorial:
+         *   We need to think ahead here, we'll be looking up for the health regularly, 
+         *   so it make sense to cache a reference of the PlayerManager for efficiency.
+         */
+        private PlayerManager target;
+
+        float characterControllerHeight = 0f;
+        Transform targetTransform;
+        Vector3 targetPosition;
+
+
+        #endregion
+
+
+        #region MonoBehaviour Callbacks
+
+        void Awake()
+        {
+            /** Note from tutorial:
+             *   Why going brute force and find the Canvas this way? Because when scenes are going 
+             *   to be loaded and unloaded, so is our Prefab, and the Canvas will be everytime different. To avoid more complex 
+             *   code structure, we'll go for the quickest way. However it's really not recommended to use "Find", because this is a 
+             *   slow operation. This is out of scope for this tutorial to implement a more complex handling of such case, but a 
+             *   good exercise when you'll feel comfortable with Unity and scripting to find ways into coding a better management 
+             *   of the reference of the Canvas element that takes loading and unloading into account.
+             */
+            this.transform.SetParent(GameObject.Find("Canvas").GetComponent<Transform>(), false);
+
+
+        }
+
+        void Update()
+        {
+            // Reflect the Player Health
+            if (playerHealthSlider != null)
+            {
+                playerHealthSlider.value = target.Health;
+            }
+
+            /** Notes from tutorial:
+             *   This code, while easy, is actually quite handy. Because of the way Photon deletes 
+             *   Instances that are networked, it's easier for the UI instance to simply destroy itself if the target reference is null. 
+             *   This avoids a lot of potential problems, and is very secure, no matter the reason why a target is missing, the related 
+             *   UI will automatically destroy itself too, very handy and quick.
+             */
+            // My note paraphrasing tutorial: it's so we don't have orphaned UIs when a player leaves the game
+            // Destroy itself if the target is null, It's a fail safe when Photon is destroying Instances of a Player over the network
+            if (target == null)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+        }
+
+        void LateUpdate()
+        {
+            /** Note from tutorial:
+             *   So, the trick to match a 2D position with a 3D position is to use the WorldToScreenPoint function of a camera and 
+             *   since we only have one in our game, we can rely on accessing the Main Camera which is the default setup for a Unity 
+             *   Scene.
+             *   Notice how we setup the offset in several steps: first we get the actual position of the target, then we add the 
+             *   characterControllerHeight, and finally, after we've deduced the screen position of the top of the Player, we add the 
+             *   screen offset.
+             */
+            // #Critical
+            // Follow the Target GameObject on screen.
+            if (targetTransform != null)
+            {
+                targetPosition = targetTransform.position;
+                targetPosition.y += characterControllerHeight;
+                this.transform.position = Camera.main.WorldToScreenPoint(targetPosition) + screenOffset;
+            }
+        }
+
+
+        #endregion
+
+
+        #region Public Methods
+
+        /** My note:
+         *   - This function is called in two places in PlayerManager after PlayerUiPrefab instantiation with this code:
+         *     _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+         *   - _uiGo represents a user interface game object
+         */
+        public void SetTarget(PlayerManager _target)
+        {
+            if (_target == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> PlayMakerManager target for PlayerUI.SetTarget.", this);
+                return;
+            }
+            // Cache references for efficiency
+            target = _target;
+            if (playerNameText != null)
+            {
+                playerNameText.text = target.photonView.Owner.NickName;
+            }
+            
+            /** Note from tutorial:
+             *   We know our player to be based off a CharacterController, which features a Height property, we'll need 
+             *   this to do a proper offset of the UI element above the Player.
+             */
+            CharacterController characterController = _target.GetComponent<CharacterController>();
+            // Get data from the Player that won't change during the lifetime of this Component
+            if (characterController != null)
+            {
+                characterControllerHeight = characterController.height;
+            }
+        }
+
+
+        #endregion
+
+
+    }
+}
