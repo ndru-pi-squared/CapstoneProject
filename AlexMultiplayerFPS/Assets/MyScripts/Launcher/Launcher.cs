7cddc8c044e995e579eb24fa4c55231124fea764
tua58997 <tua58997@temple.edu>
2019-03-06 23:17:47 -0500

    user can now join or create a specific room by name

--- AlexMultiplayerFPS/Assets/MyScripts/Launcher/Launcher.cs
+++ AlexMultiplayerFPS/Assets/MyScripts/Launcher/Launcher.cs
@@ -23,6 +23,9 @@ namespace Com.Kabaj.TestPhotonMultiplayerFPSGame
         private GameObject progressLabel;
 
         [SerializeField]
+        private InputField roomNameInputField;
+
+        [SerializeField]
         private ScrollRect existingRoomList;
         
         #endregion
@@ -43,6 +46,7 @@ namespace Com.Kabaj.TestPhotonMultiplayerFPSGame
         /// Typically this is used for the OnConnectedToMaster() callback.
         /// </summary>
         bool isConnecting;
+        bool isJoiningNamedRoom = false;
 
         #endregion
 
@@ -77,7 +81,7 @@ namespace Com.Kabaj.TestPhotonMultiplayerFPSGame
         /// </summary>
         void Start()
         {
-            progressLabel.SetActive(false);
+            //progressLabel.SetActive(false);
             controlPanel.SetActive(true);
             
         }
@@ -113,11 +117,14 @@ namespace Com.Kabaj.TestPhotonMultiplayerFPSGame
         /// </summary>
         public void Connect()
         {
+            Debug.Log("Launcher: Connect()");
             //keep track of the will to join a room, because when we come back from the game we will get a callback that we are connected, so we need to know what to do then
             isConnecting = true;
 
-            progressLabel.SetActive(true);
-            controlPanel.SetActive(false);
+            progressLabel.GetComponent<Text>().text = "Connecting...";
+
+            //progressLabel.SetActive(true);
+            //controlPanel.SetActive(false);
 
             /** My Note:
              *   I'm going to try to force connection to 'us' region for testing purposes.
@@ -128,7 +135,7 @@ namespace Com.Kabaj.TestPhotonMultiplayerFPSGame
              *   to be a good solution for testing but would need to change before publishing.
              */
 
-            // we check if we are connected or not, we join if we are, else we initiate the connection to the server.
+            /*// we check if we are connected or not, we join if we are, else we initiate the connection to the server.
             if (PhotonNetwork.IsConnected)
             {
                 // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
@@ -139,9 +146,77 @@ namespace Com.Kabaj.TestPhotonMultiplayerFPSGame
                 // #Critical, we must first and foremost connect to Photon Online Server.
                 PhotonNetwork.GameVersion = gameVersion;
                 PhotonNetwork.ConnectUsingSettings();
+            }*/
+
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public void JoinRandomRoom()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                Debug.Log("Launcher: JoinRandomRoom()");
+                progressLabel.GetComponent<Text>().text = "Joining Random Room...";
+                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                Connect();
             }
         }
 
+        /// <summary>
+        /// Join or create room without expected users
+        /// </summary>
+        public void JoinOrCreateRoom()
+        {
+            // Join or create room (with no expected users)
+            JoinOrCreateRoom(null);
+        }
+
+        /// <summary>
+        /// Join or create room with expected users
+        /// </summary>
+        /// <param name="expectedUsers">The expected users.</param>
+        public void JoinOrCreateRoom(string[] expectedUsers)
+        {
+            Debug.Log("Launcher: JoinOrCreateRoom(string[] expectedUsers)");
+            progressLabel.GetComponent<Text>().text = "Joining or Creating Room...";
+
+            // In case we are not yet connected, keep track of what we're trying to do with isJoiningNamedRoom...
+            // We will try to connect first. Then this function will be called again.
+            isJoiningNamedRoom = true;
+
+            // Check that we are connected to master server before we attempt to join a room.
+            // If we're not connected to a master server...
+            if (!PhotonNetwork.IsConnected)
+            {
+                // 
+                progressLabel.GetComponent<Text>().text = "Not yet connected!";
+                Debug.Log("Launcher: JoinOrCreateRoom(string[] expectedUsers) Not yet connected! ");
+                // Try to connect
+                Connect();
+                // Do not continue to trying to join a room. We will wait for the OnConnectedToMaster callback to call this function again
+                return;
+            }
+
+            // Get the roomName from the GUI input field
+            string roomName = roomNameInputField.text;
+            
+            // Create a new RoomOptions
+            RoomOptions roomOptions = new RoomOptions();
+            
+            // Set room options
+            //roomOptions.CustomRoomPropertiesForLobby = { "map", "ai" };
+            //roomOptions.CustomRoomProperties = new Hashtable() { { "map", 1 } };
+            //roomOptions.MaxPlayers = expectedMaxPlayers;
+            
+            // Join or create room (w/out expected users)
+            PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default, expectedUsers);
+        }
 
         #endregion
 
@@ -168,29 +243,43 @@ namespace Com.Kabaj.TestPhotonMultiplayerFPSGame
         public override void OnConnectedToMaster()
         {
             Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+            progressLabel.GetComponent<Text>().text = "Connected to Master server!";
 
             // we don't want to do anything if we are not attempting to join a room.
             // this case where isConnecting is false is typically when you lost or quit the game, when this level is loaded, OnConnectedToMaster will be called, in that case
             // we don't want to do anything.
             if (isConnecting)
             {
-                //#Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnJoinRandomFailed()
-                PhotonNetwork.JoinRandomRoom();
+                // If user wanted to join a specific room but was not yet connected to master server...
+                // (the JoinOrCreateRoom() function will have set isJoiningNamedRoom to true, tried to connect to master server, and then returned)
+                if (isJoiningNamedRoom)
+                {
+                    // We need to call JoinOrCreateRoom back to finish its work
+                    JoinOrCreateRoom();
+                }
+                // If user did not want to join a specific room...
+                else
+                {
+                    //#Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnJoinRandomFailed()
+                    PhotonNetwork.JoinRandomRoom();
+                }
             }
         }
 
         public override void OnDisconnected(DisconnectCause cause)
         {
-            progressLabel.SetActive(false);
-            controlPanel.SetActive(true);
+            //progressLabel.SetActive(false);
+            //controlPanel.SetActive(true);
 
             Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnect() was called by PUN with reason {0}", cause);
+            progressLabel.GetComponent<Text>().text = string.Format("Disconnected: {0}", cause);
         }
 
         public override void OnJoinRandomFailed(short returnCode, string message)
         {
             Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
-
+            progressLabel.GetComponent<Text>().text = "No random room available, so we create one....";
+            
             // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
             PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
         }
@@ -198,12 +287,13 @@ namespace Com.Kabaj.TestPhotonMultiplayerFPSGame
         public override void OnJoinedRoom()
         {
             Debug.Log("PUN Basicis Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+            progressLabel.GetComponent<Text>().text = "Now this client is in a room!";
             
             // #Critical: We only load if we are the first player, else we rely on 'PhotonNetwork.AutomaticallySyncScene' to sync our instance scene.
             if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
             {
-                Debug.Log("We load the 'Room for 1' ");
-
+                Debug.Log("We load the 'Room for 1'");
+                progressLabel.GetComponent<Text>().text = "Loading the 'Room for 1' map...";
 
                 // #Critical
                 // Load the Room Level.
