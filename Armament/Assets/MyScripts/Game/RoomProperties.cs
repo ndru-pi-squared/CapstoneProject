95190598723ca3dc6805fdf9aaf784679b6813e4
tua58997 <tua58997@temple.edu>
2019-03-10 11:10:33 -0400

    Forgot to add file to previous commit

---
+++ Armament/Assets/MyScripts/Game/RoomProperties.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Kabaj.TestPhotonMultiplayerFPSGame
+{
+    /// <summary>
+    /// Helps us store custom properties for game room
+    /// <para>Example properties:</para>
+    /// <para>- Number of Players on Team A</para>
+    /// <para>- Number of Players Team B</para>
+    /// </summary>
+    public class RoomProperties : MonoBehaviour
+    {
+        public const string KEY_TEAM_A_PLAYERS_COUNT = "Team A Size";
+        public const string KEY_TEAM_B_PLAYERS_COUNT = "Team B Size";
+
+        public ExitGames.Client.Photon.Hashtable Properties { get; set; } // the properties hashtable given to Photon's Room.SetCustomProperties method
+
+        private void Awake()
+        {
+            Properties = new ExitGames.Client.Photon.Hashtable();
+            //Properties.Add(KEY_TEAM_A_PLAYERS_COUNT, 0);
+            //Properties.Add(KEY_TEAM_B_PLAYERS_COUNT, 0);
+        }
+
+    }
+}
