6bd57dbc4a78dec9327a801b362820b335b518d6
Jeremy Scott <jeremy.scott@icona.local>
2019-03-17 20:11:06 -0400

    New arena

---
+++ Armament/Assets/MyScripts/Game/Hook.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    #region Public Fields
+    public Camera fpsCam;
+    #endregion Public Fields
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
