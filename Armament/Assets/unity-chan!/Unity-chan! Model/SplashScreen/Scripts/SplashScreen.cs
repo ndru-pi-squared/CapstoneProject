3b00332f22e981ab8377332a2af11398edb938a8
ndru-pi-squared <tug84943@temple.edu>
2019-03-22 09:41:56 -0400

    Added the Unity-chan character model
    Initially there were a bunch of warnings generated (and 1 error). I corrected the error and fixed a few of the warnings. But after I ran the game to test, the remaining warnings disappeared and I cannot find a way to recreate them. Nothing depends on them so it should be ok for now but I would like to clear those warnings if possible.

---
+++ Armament/Assets/unity-chan!/Unity-chan! Model/SplashScreen/Scripts/SplashScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+	[ExecuteInEditMode]
+	public class SplashScreen : MonoBehaviour
+	{
+		void NextLevel ()
+		{
+			//SceneManager.LoadScene (Application.loadedLevel + 1);
+		}
+	}
+}
