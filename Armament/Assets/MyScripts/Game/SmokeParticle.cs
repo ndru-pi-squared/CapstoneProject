ad7c3a279cb0b7a4c00577281325b195fea659ba
ndru-pi-squared <tug84943@temple.edu>
2019-03-21 22:12:19 -0400

    Press C to activate your Elon Musk power
    Added a super dumb 'AI' car. The player can summon it by pressing C to bump into stuff and make right turns. It has a gradient smoke particle effect coming out of the tailpipe.
    
    Thinking about the car helped sift out super dumb AI, how we will use various item types (we can just swap out the car for another item). As of now if you press c too many times, the game will lag from so many cars. So after about 5 seconds they should be destroyed. I would like to have them do a fade out effect so that they disappear smoothly like a spectral car.

---
+++ Armament/Assets/MyScripts/Game/SmokeParticle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeParticle : MonoBehaviour
+{
+    private ParticleSystem ps;
+    public float hSliderValueR = 0.0F;
+    public float hSliderValueG = 0.0F;
+    public float hSliderValueB = 0.0F;
+    public float hSliderValueA = 1.0F;
+    public Gradient g;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParticleSystem.MainModule psMain = ps.main;
+        hSliderValueR++;
+        hSliderValueG++;
+        hSliderValueB++;
+        hSliderValueA++;
+        psMain.startColor = g.Evaluate(Random.Range(0f, 1f));
+        ps.Emit(1);
+    }
+}
