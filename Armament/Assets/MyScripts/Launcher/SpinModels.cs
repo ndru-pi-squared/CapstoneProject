712097730b83cb39d2344117527db8d69676ac40
ndru-pi-squared <tug84943@temple.edu>
2019-03-24 05:47:15 -0400

    Added Kyle Robot and Unity Chan models to the Launcher UI

---
+++ Armament/Assets/MyScripts/Launcher/SpinModels.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinModels : MonoBehaviour
+{
+    [SerializeField] private GameObject kyleRobotModel;
+    [SerializeField] private GameObject unityChanModel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        kyleRobotModel.transform.Rotate(0, 1, 0);
+        unityChanModel.transform.Rotate(0, 1, 0);
+    }
+}
