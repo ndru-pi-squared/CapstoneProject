1870c4f5ea3d28cc7fb2b1970dea7b6a5cf3a199
Ben Mankin` <trinitycjm@yahoo.com>
2019-04-01 16:36:01 -0400

    New Launcher Scene

---
+++ Armament/Assets/MyScripts/CameraAnimationHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimationHandler : MonoBehaviour
+{
+    public static CameraAnimationHandler CA;
+    public Animator anim;
+
+    void Awake() {
+        CA = this;
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start() {}
+
+    // Update is called once per frame
+    void Update() {
+        if (anim.IsInTransition(0)) {
+            ResetTriggers();
+        }
+
+    }
+
+    public void MoveLeft() {
+        anim.SetTrigger("MoveLeft");
+    }
+
+    public void MoveRight() {
+        anim.SetTrigger("MoveRight");
+    }
+
+    public void MoveBack() {
+        anim.SetTrigger("MoveBack");
+    }
+
+    public void MoveUp() {
+        anim.SetTrigger("MoveUp");
+    }
+
+    public void ResetTriggers() {
+        anim.ResetTrigger("MoveLeft");
+        anim.ResetTrigger("MoveRight");
+        anim.ResetTrigger("MoveBack");
+        anim.ResetTrigger("MoveUp");
+    }
+}
