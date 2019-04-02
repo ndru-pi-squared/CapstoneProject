5ac41d6bd1e62839a109617846170ee4020c3a03
Andrew <tug84943@temple.edu>
2019-04-02 16:48:03 -0400

    Replaced the RoombaCar model with a grenade model.
    You can now press C to slide grenades across the floor with poor physics (no explosion yet)

--- Armament/Assets/MyScripts/Game/AICar.cs
+++ Armament/Assets/MyScripts/Game/AICar.cs
@@ -28,6 +28,8 @@ public class AICar : MonoBehaviour
             //Debug.Log("What is the thing AI car hit: " + p.name);
             //if p.name is player | FPScamp | whatever it is for a player TODO: build out and check what the player name attribute is actually set to in debugger
             //p.getComponent<PlayerManager>().TakeDamage(10f, p.GetComponent<PlayerManager>());  //something like that
-            this.gameObject.transform.Rotate(new Vector3(0, 90, 0));//only turn right
+            
+            //previously this was the only line not commented out. commented it out for the swap to a grenade:
+            //this.gameObject.transform.Rotate(new Vector3(0, 90, 0));//only turn right
     }
 }
